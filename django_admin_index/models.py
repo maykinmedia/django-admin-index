5d10238ad8db031a20a972d26f2621a2aeda7ff8
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2017-11-03 10:27:04 +0100

    Fixed isort in models.py

--- django_admin_index/models.py
+++ django_admin_index/models.py
@@ -7,7 +7,6 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.text import capfirst
 from django.utils.translation import ugettext_lazy as _
-
 from ordered_model.models import OrderedModel
 
 from . import settings
