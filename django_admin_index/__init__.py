8493003884f7acfcc75a3da069e872afae96c97a
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2018-02-28 16:57:37 +0100

    Fixed a bug with the slug creation

--- django_admin_index/__init__.py
+++ django_admin_index/__init__.py
@@ -10,7 +10,7 @@ import re
 
 from collections import namedtuple
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Joeri Bekker'
 __contact__ = 'joeri@maykinmedia.nl'
 __homepage__ = 'https://github.com/maykinmedia/django-admin-index'
