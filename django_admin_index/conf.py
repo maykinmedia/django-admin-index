88ddf8da24756aa99c26799eb2629658fdc1dab1
Joeri Bekker <joeri@maykinmedia.nl>
2019-10-18 15:38:38 +0200

    :rotating_light: Made an easy setting to see if misc apps should be shown.

--- django_admin_index/conf.py
+++ django_admin_index/conf.py
@@ -12,6 +12,12 @@ class Settings:
     def SHOW_REMAINING_APPS_TO_SUPERUSERS(self):
         return getattr(django_settings, 'ADMIN_INDEX_SHOW_REMAINING_APPS_TO_SUPERUSERS', True)
 
+    def show_remaining_apps(self, is_superuser):
+        if self.SHOW_REMAINING_APPS:
+            return True
+
+        return settings.SHOW_REMAINING_APPS_TO_SUPERUSERS and is_superuser
+
     @property
     def AUTO_CREATE_APP_GROUP(self):
         return getattr(django_settings, 'ADMIN_INDEX_AUTO_CREATE_APP_GROUP', False)
