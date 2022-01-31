5c175cd9621c418f6c53c3e5b13ad37ca3912463
Dario Mory <dario@mory.dev>
2022-01-31 17:41:34 +0100

    :alien: Use `gettext_lazy` function — #47

--- django_admin_index/apps.py
+++ django_admin_index/apps.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.apps import AppConfig, apps
 from django.core.checks import Error, Tags, Warning, register
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 __all__ = ["AdminIndexConfig"]
 
