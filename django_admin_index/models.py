72e3a61fe884b9555df9bd040c07985152a85767
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-14 17:49:25 +0100

    :wrench: Fix isort config to be black compatible

--- django_admin_index/models.py
+++ django_admin_index/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
+
 from ordered_model.models import OrderedModel, OrderedModelQuerySet
 
 from .conf import settings
