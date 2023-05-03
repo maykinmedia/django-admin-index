a4edaa2abd83149fe5cc66f100b6b1dc54005df1
Sergei Maertens <sergei@maykinmedia.nl>
2023-05-03 09:51:00 +0200

    :art: Format with latest black version

--- django_admin_index/migrations/0004_auto_20230503_0723.py
+++ django_admin_index/migrations/0004_auto_20230503_0723.py
@@ -4,20 +4,23 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('admin_index', '0003_auto_20200724_1516'),
+        ("admin_index", "0003_auto_20200724_1516"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='appgroup',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="appgroup",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
         migrations.AlterField(
-            model_name='applink',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="applink",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
