bac527421f359aa6b9879bdedebf95c9ebd6eae9
Viicos <65306057+Viicos@users.noreply.github.com>
2024-03-08 11:50:29 +0100

    Add support for Python 3.12, drop support for Python 3.7, Django 4.1
    Update actions versions
    
    Switch to `pyproject.toml`
    
    Fix/drop a bunch of outdated things
    
    Run flake8

--- django_admin_index/__init__.py
+++ django_admin_index/__init__.py
@@ -1,5 +1,7 @@
 # :copyright: (c) 2017, Maykin Media BV.
 #             All rights reserved.
 # :license:   BSD (3 Clause), see LICENSE for more details.
-__version__ = "3.1.1"
+from importlib.metadata import version
+
+__version__ = version("django-admin-index")
 __author__ = "Joeri Bekker"
